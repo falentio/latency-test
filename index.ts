5dac0dea5bf88c49210dd1a8ecc8c8c87a7b261f
falentio <riv.kevinfalent.io@gmail.com>
2023-06-02 09:00:19 +0700

    init

---
+++ index.ts
@@ -0,0 +1,15 @@
+import postgres from "https://deno.land/x/postgresjs/mod.js"
+import { serve } from "https://deno.land/std/http/mod.ts"
+
+const sql = postgres(Deno.env.get("DATABASE_URL"))
+
+serve(async () => {
+    const start = Date.now()
+    await sql`select 1`
+    const end = Date.now()
+    return new Response("", {
+        headers: {
+            delta: (end - start).toLocaleString("id-ID")
+        }
+    })
+})
